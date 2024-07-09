0d7cf0f2261f4f4dad51d16f66a33a17ea31b609
John Angarita <jnangaritaortiz@gmail.com>
2024-07-09 13:07:14 -0500

    feat: add new component
    - add QuestionContainer.vue component to
      encapsulate the logic of listing questions

--- src/pages/home/components/ExploreQuestions.vue
+++ src/pages/home/components/ExploreQuestions.vue
@@ -24,73 +24,7 @@
         <q-btn :label="$t('makeQuestion')" :no-caps="true" color="primary" />
       </div>
     </div>
-    <div
-      v-for="question in data.questions"
-      :key="question.questionId"
-      class="question-container"
-    >
-      <q-separator></q-separator>
-      <div class="row">
-        <div class="col-3 q-pa-md">
-          <p class="response-info">{{ question.votes + " " + $t("votes") }}</p>
-          <p class="response-info">
-            <q-badge
-              v-if="question.answers !== 0"
-              :outline="!toggleBorder(question.questionStatus)"
-              class="badge-response"
-              color="positive"
-            >
-              <q-icon
-                class="q-ml-xs check-icon"
-                name="las la-check"
-                size="14px"
-                v-show="toggleBorder(question.questionStatus)"
-              />
-              <span>{{ question.answers + " " + $t("answers") }}</span>
-            </q-badge>
-            <span v-else class="opacity-info">
-              {{ question.answers + " " + $t("answers") }}
-            </span>
-          </p>
-          <p class="response-info opacity-info">
-            {{ question.views + " " + $t("views") }}
-          </p>
-        </div>
-        <div class="col-9 q-pa-md">
-          <router-link
-            :to="'/question/' + question.questionId"
-            class="question-link"
-          >
-            <p class="question-title">{{ question.questionTitle }}</p>
-          </router-link>
-          <div class="display-block">
-            <q-btn
-              v-for="category in question.categories"
-              :key="category.id"
-              :label="category.name"
-              :no-caps="true"
-              :unelevated="true"
-              class="btn-category"
-              color="grey-3"
-              dense
-              text-color="dark"
-            />
-          </div>
-          <div class="flex-end margin-top-5">
-            <q-avatar rounded size="20px">
-              <img
-                :alt="$t('userImg')"
-                src="https://cdn.quasar.dev/img/boy-avatar.png"
-              />
-            </q-avatar>
-            <div>
-              <span class="margin-left-3 info-user">{{ question.user }}</span>
-              <span>{{ question.time }}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <QuestionContainer :arrQuestions="data.questions" />
     <div class="more-questions">
       <q-btn
         :label="$t('searchMoreQuestions')"
@@ -102,20 +36,13 @@
 </template>
 
 <script setup>
-import { constants } from "src/utils/constants";
 import { onMounted } from "vue";
 import { useGetData } from "src/composables/useGetData";
+import QuestionContainer from "src/pages/home/components/QuestionContainer.vue";
 
 const API_GET_QUESTIONS = "/home/question.json";
 const API_GET_CATEGORIES = "/home/category.json";
 const { data, getData } = useGetData();
-
-const toggleBorder = (questionStatus) => {
-  return (
-    questionStatus !== undefined &&
-    questionStatus === constants.QUESTION_ANSWERED
-  );
-};
 
 onMounted(() => {
   getData(API_GET_QUESTIONS, "questions");
---
+++ src/pages/home/components/QuestionContainer.vue
@@ -0,0 +1,85 @@
+<template>
+  <div
+    v-for="question in arrQuestions"
+    :key="question.questionId"
+    class="question-container"
+  >
+    <q-separator></q-separator>
+    <div class="row">
+      <div class="col-3 q-pa-md">
+        <p class="response-info">{{ question.votes + " " + $t("votes") }}</p>
+        <p class="response-info">
+          <q-badge
+            v-if="question.answers !== 0"
+            :outline="!toggleBorder(question.questionStatus)"
+            class="badge-response"
+            color="positive"
+          >
+            <q-icon
+              class="q-ml-xs check-icon"
+              name="las la-check"
+              size="14px"
+              v-show="toggleBorder(question.questionStatus)"
+            />
+            <span>{{ question.answers + " " + $t("answers") }}</span>
+          </q-badge>
+          <span v-else class="opacity-info">
+            {{ question.answers + " " + $t("answers") }}
+          </span>
+        </p>
+        <p class="response-info opacity-info">
+          {{ question.views + " " + $t("views") }}
+        </p>
+      </div>
+      <div class="col-9 q-pa-md">
+        <router-link
+          :to="'/question/' + question.questionId"
+          class="question-link"
+        >
+          <p class="question-title">{{ question.questionTitle }}</p>
+        </router-link>
+        <div class="display-block">
+          <q-btn
+            v-for="category in question.categories"
+            :key="category.id"
+            :label="category.name"
+            :no-caps="true"
+            :unelevated="true"
+            class="btn-category"
+            color="grey-3"
+            dense
+            text-color="dark"
+          />
+        </div>
+        <div class="flex-end margin-top-5">
+          <q-avatar rounded size="20px">
+            <img
+              :alt="$t('userImg')"
+              src="https://cdn.quasar.dev/img/boy-avatar.png"
+            />
+          </q-avatar>
+          <div>
+            <span class="margin-left-3 info-user">{{ question.user }}</span>
+            <span>{{ question.time }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { constants } from "src/utils/constants";
+import { defineProps } from "vue";
+
+const props = defineProps({
+  arrQuestions: {
+    type: Array,
+    required: true,
+  },
+});
+
+const toggleBorder = (questionStatus) => {
+  return questionStatus === constants.QUESTION_ANSWERED;
+};
+</script>
