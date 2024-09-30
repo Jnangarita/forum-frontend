ecb74b251c921e289f26c1fef140ee51ecc0a2ea
John Angarita <jnangaritaortiz@gmail.com>
2024-09-30 12:27:29 -0500

    refactor: update useAuthStore
    -  update useAuthStore to modern pinia syntax

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,40 +1,50 @@
 import { authLogin } from 'src/api/authentication';
 import { defineStore } from 'pinia';
-import { HttpStatusCode } from "axios";
-import { jwtDecode } from "jwt-decode";
+import { HttpStatusCode } from 'axios';
+import { jwtDecode } from 'jwt-decode';
+import { ref, computed } from 'vue';
 import { util } from 'src/utils/functions';
 
-export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    authToken: null,
-    isAuthenticated: false,
-    user: null,
-  }),
+export const useAuthStore = defineStore('auth', () => {
+  const authToken = ref(null);
+  const isAuthenticated = ref(false);
+  const user = ref(null);
 
-  actions: {
-    async login(credentials) {
-      const response = await authLogin.authUser(credentials);
-      if (response.status === HttpStatusCode.Ok) {
-        this.authToken = response.data.jwtToken;
-        this.isAuthenticated = true;
-        localStorage.setItem('authToken', this.authToken);
-        this.user = jwtDecode(this.authToken);
-        localStorage.setItem('userId', this.user.id);
-      }
-    },
+  const login = async (credentials) => {
+    const response = await authLogin.authUser(credentials);
+    if (response.status === HttpStatusCode.Ok) {
+      authToken.value = response.data.jwtToken;
+      isAuthenticated.value = true;
+      localStorage.setItem('authToken', authToken.value);
+      user.value = jwtDecode(authToken.value);
+      localStorage.setItem('userId', user.value.id);
+    }
+  };
 
-    logout() {
-      this.isAuthenticated = false;
-      this.user = null;
-      this.authToken = null;
-      util.storage.deleteInfoLocalStorage();
-      util.storage.deleteStore();
-    },
-  },
+  const logout = () => {
+    deleteInfo();
+    util.storage.deleteInfoLocalStorage();
+    util.storage.deleteStore();
+  };
 
-  getters: {
-    authenticatedUser: (state) => state.authToken !== null,
-    userInfo: (state) => state.user,
-    authenticatedJwtToken: (state) => state.authToken,
-  },
+  const deleteInfo = () => {
+    isAuthenticated.value = false;
+    user.value = null;
+    authToken.value = null;
+  };
+
+  const authenticatedUser = computed(() => authToken.value !== null);
+  const userInfo = computed(() => user.value);
+  const authenticatedJwtToken = computed(() => authToken.value);
+
+  return {
+    login,
+    logout,
+    authenticatedJwtToken,
+    authenticatedUser,
+    authToken,
+    isAuthenticated,
+    user,
+    userInfo,
+  };
 });
